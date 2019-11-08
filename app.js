9fac3c936fb8236041d42e8d03724244fd49e963
suyuan <suyuan326@qq.com>
2019-11-08 13:39:25 +0800

    封装http

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-const AV = require('./lib/av-weapp-min.js');
 App({
   globalData: {
-    host: "https://gp-server.hunger-valley.com",
-    app_id: "wx002c39c0187e860e",
-    app_secret: "490f0375bea23fe0dcbf787f27c2c904"
+
   }
 })
