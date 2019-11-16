4c40c616b7b150129d624e331e7a2e115f7f5c99
suyuan <suyuan326@qq.com>
2019-11-16 10:34:30 +0800

    封装http

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 App({
   globalData: {
-
+    host: "https://gp-server.hunger-valley.com",
+    app_id: "wx002c39c0187e860e",
+    app_secret: "490f0375bea23fe0dcbf787f27c2c904",
+    t_app_id: "bBDcBaPiXHgcr2smK53xYB7s",
+    t_app_secret: "G291oG7BhHhSFQ6G9aQrSdXG"
   }
 })
